9ed54bdf8a5ad5b58826278557bc1d9d6b8b36a6
KT <kiranmayi1609@gmail.com>
2024-11-01 09:18:35 +0100

    Build Angualr portfolio for Github Page deployment

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-V4TSPYHS.mjs";import"./chunk-EMACFE4E.mjs";import"./chunk-YKHV6Y4L.mjs";import"./chunk-CWG6TGYU.mjs";import"./chunk-RFUFOMYI.mjs";import"./chunk-YTPJDBFP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
